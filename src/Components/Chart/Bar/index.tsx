a22fda670374d1af1ea06c5e615f7ddd7400e5ed
ProgrammerFront077 <alirezakh0090@gmail.com>
2023-09-10 12:56:53 +0430

    (Add):<-Bar Chart->

--- src/Components/Chart/Bar/index.tsx
+++ src/Components/Chart/Bar/index.tsx
@@ -1,5 +1,55 @@
-const BarCharts = () => {
-  return <></>;
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts';
+import { SalesState } from '../../../Model/Product';
+import React from 'react';
+
+interface BarProps {
+  data: SalesState;
+}
+
+const BarChartComponent: React.FC<BarProps> = ({ data }) => {
+  return (
+    <>
+      <h1 className="capitalize text-4xl">bar chart</h1>
+      <ResponsiveContainer width="100%" height={400}>
+        <BarChart data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="year" label={{ value: 'Year', position: 'bottom' }} />
+          <YAxis
+            label={{ value: 'Sales', angle: -90, position: 'insideLeft' }}
+          />
+          <Tooltip />
+          <Legend verticalAlign="top" align="center" />
+          <Bar
+            dataKey="shirt"
+            name="Shirt Sales"
+            fill="#17bfd9"
+            label={{ fill: 'black', fontSize: 10 }}
+          />
+          <Bar
+            dataKey="pants"
+            name="Pants Sales"
+            fill="#ff7300"
+            label={{ fill: 'black', fontSize: 10 }}
+          />
+          <Bar
+            dataKey="shoe"
+            name="Shoe Sales"
+            fill="#ffc658"
+            label={{ fill: 'black', fontSize: 10 }}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </>
+  );
 };
 
-export default BarCharts;
+export default BarChartComponent;
--- src/Components/Chart/Line/index.tsx
+++ src/Components/Chart/Line/index.tsx
@@ -1,6 +1,14 @@
 import { LineChart } from 'recharts';
 import { SalesState } from '../../../Model/Product';
-import { XAxis, YAxis, Tooltip, CartesianGrid, Legend, Line } from 'recharts';
+import {
+  XAxis,
+  YAxis,
+  Tooltip,
+  CartesianGrid,
+  Legend,
+  Line,
+  ResponsiveContainer,
+} from 'recharts';
 import React from 'react';
 
 interface LineProps {
@@ -9,44 +17,47 @@ interface LineProps {
 
 const LineCharts: React.FC<LineProps> = ({ data }) => {
   return (
-    <section className="flex flex-col space-y-5">
+    <>
       <h1 className="capitalize text-4xl">line chart</h1>
-
-      <LineChart width={800} height={500} data={data}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="year" label={{ value: 'Year', position: 'bottom' }} />
-        <YAxis label={{ value: 'Sales', angle: -90, position: 'insideLeft' }} />
-        <Tooltip />
-        <Legend verticalAlign="top" align="center" />
-        <Line
-          type="monotone"
-          dataKey="shirt"
-          name="Shirt Sales"
-          stroke="#17bfd9"
-          dot={{ stroke: 'red', strokeWidth: 2, r: 1 }}
-          label={{ fill: 'black', fontSize: 10 }}
-          strokeDasharray="4 2"
-        />
-        <Line
-          type="monotone"
-          dataKey="pants"
-          name="Pants Sales"
-          stroke="#ff7300"
-          dot={{ stroke: 'blue', strokeWidth: 2, r: 1 }}
-          label={{ fill: 'black', fontSize: 10 }}
-          strokeDasharray="4 2"
-        />
-        <Line
-          type="monotone"
-          dataKey="shoe"
-          name="Shoe Sales"
-          stroke="#ffc658"
-          dot={{ stroke: 'green', strokeWidth: 2, r: 1 }}
-          label={{ fill: 'black', fontSize: 10 }}
-          strokeDasharray="4 2"
-        />
-      </LineChart>
-    </section>
+      <ResponsiveContainer width="100%" height={400}>
+        <LineChart data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="year" label={{ value: 'Year', position: 'bottom' }} />
+          <YAxis
+            label={{ value: 'Sales', angle: -90, position: 'insideLeft' }}
+          />
+          <Tooltip />
+          <Legend verticalAlign="top" align="center" />
+          <Line
+            type="monotone"
+            dataKey="shirt"
+            name="Shirt Sales"
+            stroke="#17bfd9"
+            dot={{ stroke: 'red', strokeWidth: 2, r: 1 }}
+            label={{ fill: 'black', fontSize: 10 }}
+            strokeDasharray="4 2"
+          />
+          <Line
+            type="monotone"
+            dataKey="pants"
+            name="Pants Sales"
+            stroke="#ff7300"
+            dot={{ stroke: 'blue', strokeWidth: 2, r: 1 }}
+            label={{ fill: 'black', fontSize: 10 }}
+            strokeDasharray="4 2"
+          />
+          <Line
+            type="monotone"
+            dataKey="shoe"
+            name="Shoe Sales"
+            stroke="#ffc658"
+            dot={{ stroke: 'green', strokeWidth: 2, r: 1 }}
+            label={{ fill: 'black', fontSize: 10 }}
+            strokeDasharray="4 2"
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </>
   );
 };
 
